606ab2e9e7772d08c69f6f735783223780cb033d
ManWithNoName <hazzardwebsmithing@gmail.com>
2025-04-24 15:48:08 -0400

    jg

---
+++ website/v2/scripts/loadComponents.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // Load the navigation
+    $('#nav-container').load('components/nav.html');
+
+    // Load the App Page
+    $('#gsd-app-container').load('https://g-s-d.netlify.app')
+
+    // Load the footer
+    $('#footer-container').load('components/footer.html');
+});
