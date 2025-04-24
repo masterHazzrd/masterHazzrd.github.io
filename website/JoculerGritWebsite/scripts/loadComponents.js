6354e235dc840109a7f7a99b154bc12352388ab8
ManWithNoName <hazzardwebsmithing@gmail.com>
2025-04-24 16:56:05 -0400

    JG

--- website/JoculerGritWebsite/scripts/loadComponents.js
+++ website/JoculerGritWebsite/scripts/loadComponents.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
     // Load the navigation
     $('#nav-container').load('components/nav.html');
 
-    // Load the App Page
-    $('#gsd-app-container').load('https://g-s-d.netlify.app')
-
     // Load the footer
     $('#footer-container').load('components/footer.html');
 });
